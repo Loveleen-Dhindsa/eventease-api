96948b4261fc47259105518ee6263b7ceaa7fba2
lavu <lovelydhindsa44@gmail.com>
2024-06-09 03:03:41 +0530

    Updated mongoose

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var cors = require("cors");
 // });
 mongoose.connect(`mongodb://127.0.0.1:27017/eventease_db`);
 
+console.log("mongoose-------", process.env.PORT)
+
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
